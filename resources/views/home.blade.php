1dc430e82653a74fedcc3766efb1f42338832d25
mrcuong <cuongtv@haposoft.com>
2022-06-29 17:41:10 +0700

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -168,9 +168,9 @@
                     “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
                 </div>
                 <div class="slider-user">
-                    <img src="/images/avatar.png" alt="" class="user-avatar">
+                    <img src="{{asset('images/avatar.png')}}" alt="" class="user-avatar">
                     <div class="user-info">
-                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-name">Cuong TV</div>
                         <div class="user-language">PHP</div>
                         <div class="user-stars">
                             <i class="fa-solid fa-star user-star-icon"></i>
@@ -190,26 +190,7 @@
                 <div class="slider-user">
                     <img src="/images/avatar.png" alt="" class="user-avatar">
                     <div class="user-info">
-                        <div class="user-name">Hoang Anh Nguyen</div>
-                        <div class="user-language">PHP</div>
-                        <div class="user-stars">
-                            <i class="fa-solid fa-star user-star-icon"></i>
-                            <i class="fa-solid fa-star user-star-icon"></i>
-                            <i class="fa-solid fa-star user-star-icon"></i>
-                            <i class="fa-regular fa-star-half-stroke user-star-icon"></i>
-                            <i class="fa-regular fa-star user-star-icon"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slider-item">
-                <div class="slider-slogan">
-                    “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
-                </div>
-                <div class="slider-user">
-                    <img src="/images/avatar.png" alt="" class="user-avatar">
-                    <div class="user-info">
-                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-name">Cuong TV</div>
                         <div class="user-language">PHP</div>
                         <div class="user-stars">
                             <i class="fa-solid fa-star user-star-icon"></i>
@@ -228,7 +209,26 @@
                 <div class="slider-user">
                     <img src="/images/avatar.png" alt="" class="user-avatar">
                     <div class="user-info">
-                        <div class="user-name">Hoang Anh Nguyen</div>
+                        <div class="user-name">Cuong TV</div>
+                        <div class="user-language">PHP</div>
+                        <div class="user-stars">
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-solid fa-star user-star-icon"></i>
+                            <i class="fa-regular fa-star-half-stroke user-star-icon"></i>
+                            <i class="fa-regular fa-star user-star-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider-item">
+                <div class="slider-slogan">
+                    “A wonderful course on how to start. Eddie beautifully conveys all essentials of a becoming a good Angular developer. Very glad to have taken this course. Thank you Eddie Bryan.”
+                </div>
+                <div class="slider-user">
+                    <img src="/images/avatar.png" alt="" class="user-avatar">
+                    <div class="user-info">
+                        <div class="user-name">Cuong TV</div>
                         <div class="user-language">PHP</div>
                         <div class="user-stars">
                             <i class="fa-solid fa-star user-star-icon"></i>
