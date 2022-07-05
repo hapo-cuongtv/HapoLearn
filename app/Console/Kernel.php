61d58d27636f353a52c75a4823a56ba57b33a175
mrcuong <cuongtv@haposoft.com>
2022-07-05 11:42:51 +0700

    test creat branch

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,8 +8,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
+     * The Artisan commands provided by your application.git
      * @var array
      */
     protected $commands = [
